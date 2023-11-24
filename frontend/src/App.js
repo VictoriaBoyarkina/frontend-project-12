193616d1d64e5e10e41989c76b88a22c5380e39e
victoriya <victoriyaboyarkina@gmail.com>
2023-11-24 19:17:20 +0700

    4 step

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link, Navigate, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import ChatPage from './components/ChatPage.js';
 import LoginPage from './components/LoginPage';
 import NotFoundPage from './components/NotFoundPage';
 import { Button, Navbar, Nav } from 'react-bootstrap';
+import AuthContext from './contexts';
+import useAuth from './hooks/index.js';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  );
+};
 
 function App() {
   useEffect(()  => {
@@ -13,26 +40,33 @@ function App() {
       document.documentElement.classList.add('h-100')});
 
   return (
-    <BrowserRouter>
-      <div className='h-100' id='chat'>
-          <div className='d-flex flex-column h-100'>
-            <Navbar expand='lg' bg='white' className='shadow-sm navbar navbar-light'>
-              <div className='container'>
-                <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+    <AuthProvider>
+      <BrowserRouter>
+            <div className='h-100' id='chat'>
+                <div className='d-flex flex-column h-100'>
+                  <Navbar expand='lg' bg='white' className='shadow-sm navbar navbar-light'>
+                    <div className='container'>
+                      <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+                    </div>
+                  </Navbar>
+                  <div className='container-fluid h-100'>
+                    <div className='row justify-content-center align-content-center h-100'>
+                      <Routes>
+                        <Route path="/" element={(
+                          <PrivateRoute>
+                            <ChatPage/>
+                          </PrivateRoute>
+                        )} />  
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path='*' element={<NotFoundPage />} />
+                      </Routes>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </Navbar>
-            <div className='container-fluid h-100'>
-              <div className='row justify-content-center align-content-center h-100'>
-                <Routes>
-                  <Route path="/" element={null} />  
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path='*' element={<NotFoundPage />} />
-                </Routes>
-              </div>
-            </div>
-          </div>
-        </div>
     </BrowserRouter>
+    </AuthProvider>
+    
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot }from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 document.body.innerHTML = '<div class="h-100" id="container"></div>';
 const container = document.getElementById('container')
@@ -12,8 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
